576456066e99e7c8a3aba4803458fef73fdf6c24
Lee Raulin <leeraulin@gmail.com>
2019-06-17 15:53:52 -0400

    ...

--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,52 @@
+const fs = require("fs");
+
 /** Insert a space before every capital letter preceded by a non-whitespace
  * character. */
-const camelSpace = (text: string) => text.replace(/([\S])([A-Z])/g, "$1 $2");
+module.exports.camelSpace = (text: string) =>
+  text.replace(/([\S])([A-Z])/g, "$1 $2");
 
 // Capitalize first character in string.
-const capitalize = (word: string) => word[0].toUpperCase() + word.slice(1);
+module.exports.capitalize = (word: string) =>
+  word[0].toUpperCase() + word.slice(1);
 
 // Changes camelCase text to Title Case.
-export const camelToTitle = (text: string) =>
-  camelSpace(text)
+module.exports.camelToTitle = (text: string) =>
+  module.exports
+    .camelSpace(text)
     .split(" ")
-    .map(word => capitalize(word))
+    .map((word: any) => module.exports.capitalize(word))
     .join(" ");
 
 /** Take date object and return string in format MM/DD/YYYY */
-export const formatDate = (date = new Date()) => {
+module.exports.formatDate = (date = new Date()) => {
   const year = date.getFullYear();
   const month = date.getMonth();
   const day = date.getDate();
   return `${month}/${day}/${year}`;
 };
+
+module.exports.streamWriteToFile = (text: string, file: any) => {
+  const stream = fs.createWriteStream(file, { flags: "a" });
+  stream.write(text + "\n");
+  stream.end();
+};
+
+// Return a Promise that resolves to a string containing contents of file.
+module.exports.readFile = (path: string, opts = "utf8") =>
+  new Promise<any>((res, rej) => {
+    fs.readFile(path, opts, (err: Error, text: string) => {
+      if (err) rej(err);
+      else res(text);
+    });
+  });
+
+// Return a JSON file as an object.
+module.exports.readJsonFile = async (path: any) =>
+  JSON.parse(await this.readFile(path));
+
+const test = async () => {
+  const text = await module.exports.readFile("./text.txt");
+  console.log(text);
+};
+
+test();
